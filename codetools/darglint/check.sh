ae307b398b3586076e46673ad31d0f9ab2d4b389
Aart Stuurman <astuurman@hotmail.com>
2023-09-20 13:40:51 +0200

    Added linting, formatting, mypy tools

---
+++ codetools/darglint/check.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+darglint -s sphinx ../../id_key_collections
---
+++ codetools/mypy/check.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")/../.."
+
+mypy --package id_key_collections
